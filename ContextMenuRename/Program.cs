aa83be3b529a9b4c930307443880d084c1d995aa
Mitsuriou <dylanpro.jacquemin@gmail.com>
2022-11-15 00:22:47 +0100

    Refactor + added "append" and "prepend" for random GUID. [WIP]: prepared stuff for batch renaming (check to add a mutex later).

--- ContextMenuRename/Program.cs
+++ ContextMenuRename/Program.cs
@@ -1,44 +1,77 @@
-﻿namespace ContextMenuRename
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContextMenuRename
 {
     internal class Program
     {
-        enum TargetModule
+        enum MODULE
         {
             None,
-            RandomGUID
+            RandomGUID,
+            PrependRandomGUID,
+            AppendRandomGUID,
+            RandomGUIDSameForAllFiles,
+            PrependRandomGUIDSameForAllFiles,
+            AppendRandomGUIDSameForAllFiles,
         }
 
         static void Main(string[] args)
         {
+            // Error: not enough arguments
             if (args.Length < 2)
-            {
                 System.Environment.Exit(1);
-            }
 
-            // File name
-            var fileName = args[0];
-            if (fileName.Length <= 0)
-            {
-                System.Environment.Exit(1);
-            }
-
-            // Target module
-            var targetApp = TargetModule.RandomGUID;
+            // Parse: module
+            var module = MODULE.RandomGUID;
             try
             {
-                targetApp = (TargetModule)System.Int16.Parse(args[1]);
+                module = (MODULE)short.Parse(args[0]);
             }
             catch (System.IndexOutOfRangeException)
             {
                 System.Environment.Exit(1);
             }
 
-            switch (targetApp)
+            // Parse: file(s) name(s)
+            string[] fileNames = new string[args.Length - 1];
+            for (var i = 1; i < args.Length; i++)
+                fileNames[i - 1] = args[i];
+
+            switch (module)
             {
-                case TargetModule.None:
+                case MODULE.None:
                     break;
-                case TargetModule.RandomGUID:
-                    RenameFileRandomGUID(fileName);
+                case MODULE.RandomGUID:
+                    // Error: too many files
+                    if (fileNames.Length > 1)
+                        System.Environment.Exit(1);
+
+                    RenameFileRandomGUID(fileNames.First(), GenerateGUID());
+                    break;
+                case MODULE.PrependRandomGUID:
+                    // Error: too many files
+                    if (fileNames.Length > 1)
+                        System.Environment.Exit(1);
+
+                    RenameFilePrependRandomGUID(fileNames.First(), GenerateGUID());
+                    break;
+                case MODULE.AppendRandomGUID:
+                    // Error: too many files
+                    if (fileNames.Length > 1)
+                        System.Environment.Exit(1);
+
+                    RenameFileAppendRandomGUID(fileNames.First(), GenerateGUID());
+                    break;
+                case MODULE.RandomGUIDSameForAllFiles:
+                    RenameFileRandomGUIDSameForAllFiles(fileNames);
+                    break;
+                case MODULE.PrependRandomGUIDSameForAllFiles:
+                    RenameFilePrependRandomGUIDSameForAllFiles(fileNames);
+                    break;
+                case MODULE.AppendRandomGUIDSameForAllFiles:
+                    RenameFileAppendRandomGUIDSameForAllFiles(fileNames);
                     break;
             }
 
@@ -46,15 +79,82 @@
             System.Environment.Exit(0);
         }
 
-        static bool RenameFileRandomGUID(string fileName)
+        static bool RenameFileRandomGUID(in string sourceFile, in string GUID)
         {
-            var newGUID = System.Guid.NewGuid().ToString().ToUpper();
-            var parentDirectory = System.IO.Path.GetDirectoryName(fileName);
-            var extension = System.IO.Path.GetExtension(fileName);
+            if (!File.Exists(sourceFile))
+                return false;
 
-            System.IO.File.Move(fileName, System.IO.Path.Combine(parentDirectory, newGUID + extension));
+            File.Move(
+                sourceFile,
+                Path.Combine(
+                    Path.GetDirectoryName(sourceFile),
+                    GUID + Path.GetExtension(sourceFile)
+                )
+            );
 
             return true;
+        }
+
+        static bool RenameFilePrependRandomGUID(in string sourceFile, in string GUID)
+        {
+            if (!File.Exists(sourceFile))
+                return false;
+
+            File.Move(
+                sourceFile,
+                Path.Combine(
+                    Path.GetDirectoryName(sourceFile),
+                    GUID + Path.GetFileName(sourceFile)
+                )
+            );
+
+            return true;
+        }
+
+        static bool RenameFileAppendRandomGUID(in string sourceFile, in string GUID)
+        {
+            if (!File.Exists(sourceFile))
+                return false;
+
+            File.Move(
+                sourceFile,
+                Path.Combine(
+                    Path.GetDirectoryName(sourceFile),
+                    Path.GetFileNameWithoutExtension(sourceFile) + GUID + Path.GetExtension(sourceFile)
+                )
+            );
+
+            return true;
+        }
+
+        static void RenameFileRandomGUIDSameForAllFiles(in string[] sourceFiles)
+        {
+            var commonGUID = GenerateGUID();
+
+            foreach (var sourceFile in sourceFiles)
+            {
+                RenameFileRandomGUID(sourceFile, commonGUID);
+            }
+        }
+
+        static void RenameFilePrependRandomGUIDSameForAllFiles(in string[] sourceFiles)
+        {
+            var commonGUID = GenerateGUID();
+
+            foreach (var sourceFile in sourceFiles)
+                RenameFilePrependRandomGUID(sourceFile, commonGUID);
+        }
+
+        static void RenameFileAppendRandomGUIDSameForAllFiles(in string[] sourceFiles)
+        {
+            var commonGUID = GenerateGUID();
+            foreach (var sourceFile in sourceFiles)
+                RenameFileAppendRandomGUID(sourceFile, commonGUID);
+        }
+
+        static string GenerateGUID()
+        {
+            return System.Guid.NewGuid().ToString().ToUpper();
         }
     }
 }
